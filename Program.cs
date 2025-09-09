3b72ff557cd3e3eeb6ca8105d2eac927c79675b1
Hades6578 <104776592+Hades6578@users.noreply.github.com>
2025-09-09 09:19:25 -0400

    Program.cs, contains HelloTwist code

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Tap, tap");
+Console.WriteLine("Is this thing on?");
+string yes = "yes";
+if (Console.ReadLine() == yes)
+{
+    Console.WriteLine("Oh great! ");
+}
+else
+{
+    Console.WriteLine("How did you respond to me if it wasn't on?");
+}
+Console.WriteLine("Anyways, now that we've taken care of that, who's using me today?");
+string name = Console.ReadLine();
+Console.WriteLine($"Hello, {name}!");
+Console.WriteLine($"{name}, here is the current UTC time");
+DateTime utcNow = DateTime.UtcNow;
+Console.WriteLine($"Current UTC Time: {utcNow}");
+Console.WriteLine($"I could continue pretending to be helpful, {name}");
+Console.WriteLine("But I think we both know that I'm just burning electrcity right now.");
+Console.WriteLine("Tally ho, old chum!");
