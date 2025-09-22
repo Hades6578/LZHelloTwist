6ecf92199f4c1ba774827e8d3c474f50d60e3b1a
Cianna <cll30806@hawkmail.hacc.edu>
2025-09-21 20:31:31 -0400

    Direct to repo change for week 2

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ else
 {
     Console.WriteLine("How did you respond to me if it wasn't on?");
 }
+Console.WriteLine ("Well that's odd. It seems the pull request was never approved... or something else happened. :/");
 Console.WriteLine("Anyways, now that we've taken care of that, who's using me today?");
 string name = Console.ReadLine();
 Console.WriteLine($"Hello, {name}!");
@@ -19,4 +20,5 @@ DateTime utcNow = DateTime.UtcNow;
 Console.WriteLine($"Current UTC Time: {utcNow}");
 Console.WriteLine($"I could continue pretending to be helpful, {name}");
 Console.WriteLine("But I think we both know that I'm just burning electrcity right now.");
+
 Console.WriteLine("Tally ho, old chum!");
